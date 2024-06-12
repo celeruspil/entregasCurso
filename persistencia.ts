03f0f40f5b73a87112ef563dee3d7f76575abcc2
cele_ruspil <celesteruspil22@gmail.com>
2024-06-12 02:47:40 -0300

    Logrando identificar con mas velocidad cuando no funciona el codigo y solucionando

---
+++ persistencia.ts
@@ -0,0 +1,32 @@
+import fs from 'node:fs';
+const productos: string[] = ["Leche", "Galletitas", "Harina", "Queso"];
+let contenido: string = "";
+for (let i = 0; i < productos.length; i++) {
+    contenido += `${productos[i]} `;
+}
+fs.writeFileSync('./productos.txt', contenido);
+const datosProductos = fs.readFileSync('./productos.txt', "utf8");
+const sinEspacios = datosProductos.trim()
+const datoFinal = sinEspacios.split(" ");
+console.log(datoFinal);
+
+const precios: number[] = [525, 3500, 400, 1999];
+let contenidoPrecios: string = precios.toString();
+fs.writeFileSync('./precios.txt', contenidoPrecios);
+
+for (let i = 0; i < precios.length; i++) {
+    let espacio: string = "";
+    espacio += `${precios[i]} `;
+}
+const valor = fs.readFileSync('./precios.txt', "utf8");
+const espaciosOff = valor.trim();
+const precioFinal = espaciosOff.split(",");
+console.log(precioFinal);
+
+
+
+
+
+
+
+
