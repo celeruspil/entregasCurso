698d3c9ebbdc919dbc3be010921ada1b948366b6
cele_ruspil <celesteruspil22@gmail.com>
2024-07-01 16:07:00 -0300

    asd

--- persistencia.ts
+++ persistencia.ts
@@ -1,5 +1,6 @@
 import fs from 'node:fs';
 const productos: string[] = ["Leche", "Galletitas", "Harina", "Queso"];
+const precios: number[] = [525, 3500, 400, 1999];
 let contenido: string = "";
 for (let i = 0; i < productos.length; i++) {
     contenido += `${productos[i]} `;
@@ -10,7 +11,7 @@ const sinEspacios = datosProductos.trim()
 const datoFinal = sinEspacios.split(" ");
 console.log(datoFinal);
 
-const precios: number[] = [525, 3500, 400, 1999];
+
 let contenidoPrecios: string = precios.toString();
 fs.writeFileSync('./precios.txt', contenidoPrecios);
 
